ec7176e649f4c09fb00a4a3c556caaaccaadf6ae
Matthew Nguyen <mnguyen916@gmail.com>
2012-08-04 13:14:48 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biddingly::Application.config.secret_token = '03ca917e54f28b0e81c20d1f5e54835ea5f4918a6e455fb7a4d961f6b0ddaff226e537e9d6691cc86d4879fbb4158bc84c8ed26313e83074f155f35bf1a8b011'
